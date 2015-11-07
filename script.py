7f51f5acd0a0f047016e888019b9ba673ba55992
Sam Phelps <drakethes@gmail.com>
2015-11-07 17:54:30 +0000

    Catch exception on error. Update README with example output

--- script.py
+++ script.py
@@ -9,37 +9,39 @@ apiServer = "http://192.168.1.167:8081"
 outputDonationAbove = 100;
 
 while(1):
-	r = requests.get(apiServer + "/teamgoal/21514")
-	if(r.status_code==200):
-		parsed_json = json.loads(r.content)
-
-		raised = parsed_json["raised"];
-		if(raised > total):
-			total = raised;
-			print "Latest total is $" + str(total)
-
-		sleep(1)
-	else:
-		print r.status_code
-
-
-	for p in participants:
-		r = requests.get(apiServer + "/recentDonations/"+p)
+	try:
+		r = requests.get(apiServer + "/teamgoal/21514")
 		if(r.status_code==200):
 			parsed_json = json.loads(r.content)
-			for x in range(0,len(parsed_json["recentDonations"])):
-				name = parsed_json["recentDonations"][x]["name"]
-				dollars = name.split('$')
-				#Grab the value after the $ symbol
-				if (len(dollars)>1):
-					#Throw away everything after the decimal place.
-					donation = dollars[1][:-4]
-					#Throw away any commas (999+)
-					if(int(donation.replace(",", ""))>outputDonationAbove):
-						if(name not in previouslyOutputDonations):
-							print name + " ("+p+")"
-							previouslyOutputDonations.append(name)
+
+			raised = parsed_json["raised"];
+			if(raised > total):
+				total = raised;
+				print "Latest total is $" + str(total)
+
+			sleep(1)
 		else:
 			print r.status_code
-		
+
+
+		for p in participants:
+			r = requests.get(apiServer + "/recentDonations/"+p)
+			if(r.status_code==200):
+				parsed_json = json.loads(r.content)
+				for x in range(0,len(parsed_json["recentDonations"])):
+					name = parsed_json["recentDonations"][x]["name"]
+					dollars = name.split('$')
+					#Grab the value after the $ symbol
+					if (len(dollars)>1):
+						#Throw away everything after the decimal place.
+						donation = dollars[1][:-4]
+						#Throw away any commas (999+)
+						if(int(donation.replace(",", ""))>outputDonationAbove):
+							if(name not in previouslyOutputDonations):
+								print name + " ("+p+")"
+								previouslyOutputDonations.append(name)
+			else:
+				print r.status_code
+	except requests.exceptions.ConnectionError:
+		print "opps!"	
 
